3f542db91d4f1b21bd00ef289a0ad6bad07803f7
Santos <jonas.santos@sap.com>
2021-07-16 12:37:27 -0300

    test

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+const axios = require("axios")
+
 console.log("-------------running----------")
+
+axios.get("https://raw.githubusercontent.com/jonas-duarte/jenkins-test/master/package.json")
+    .then(({ data }) => console.log(data))
